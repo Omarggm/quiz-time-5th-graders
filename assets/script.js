3c25c45c54c55eecd6399c4ff7585c65ba4c16c0
Omar <Omarggm@gmail.com>
2023-02-27 15:54:50 -0800

    init git push along with code for base html, css, js file.

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+//test to make sure javascript is linked properly
+console.log('Hello')
+
+var startBtn = document.querySelector("#start");
+
+//questions for the quiz
+function quizTime() {
+const myQuestions = [
+    {
+        question: 'what is the first letter in the alaphabet?',
+        choices: ['a', 'b', 'c','d'],
+        correctAnswer: 0,
+    },
+    {
+        question: 'what is the bigest ocean?',
+        choices : ['Atlantic', 'Pacific', 'Indian'],
+        correctAnswer: 1,
+    },
+    {
+        question: 'What is the powerhouse of a cell?',
+        choices : ['Nucleus', 'Mitochondria', 'Plasma', 'Cell Wall'],
+        correctAnswer: 2,
+    }
+
+]
+
+var score = 0;
+
+for(var i=0; i<myQuestions.length; i++ ){
+    var response = window.prompt(myQuestions[i].question);
+    if(response == myQuestions[i].correctAnswer){
+        score++;
+    }
+}
+}
+
+
+//Event listner to button
+startBtn.addEventListener('click', quizTime);
